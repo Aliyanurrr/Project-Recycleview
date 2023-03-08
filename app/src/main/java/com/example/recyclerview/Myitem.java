7834af80c3ea765029003681fc037a5c7e44e638
Aliya Nur Hanifah <aliyanurr22@gmail.com>
2023-03-08 10:02:25 +0700

    first commit

---
+++ app/src/main/java/com/example/recyclerview/Myitem.java
@@ -0,0 +1,22 @@
+package com.example.recyclerview;
+
+public class Myitem {
+
+    static int[] poster = {
+            R.drawable.new_poster_1,  R.drawable.new_poster_2,
+            R.drawable.new_poster_4,  R.drawable.new_poster_5,
+            R.drawable.new_poster_7,  R.drawable.new_poster_8
+    };
+
+    static String[] judulFilm = {
+            "Venom: Let There Be Carnage", "Jurrasic World: Dominion",
+            "Turning Red: Growing Up Beast", "Shang-Chi: Legend Of The Ten",
+            "Etternals: In The Beginning", "No Time To Die : Perfect"
+    };
+
+    static String[] tahunFilm = {
+            "2021", "2022",
+            "2021", "2022",
+            "2021", "2022",
+    };
+}
